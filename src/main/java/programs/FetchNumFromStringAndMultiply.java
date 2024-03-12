56c712accc5f10591bbf8339cd9c2dcbb8094c55
Vishnu <vishnug4881@gmail.com>
2024-03-12 13:56:45 +0530

    FetchNumFromStringAndMultiply class modified

--- src/main/java/programs/FetchNumFromStringAndMultiply.java
+++ src/main/java/programs/FetchNumFromStringAndMultiply.java
@@ -3,8 +3,18 @@ package programs;
 public class FetchNumFromStringAndMultiply {
     public static void main(String[] args) {
         String ss = "This 35 is vishnu 45 ";
-        System.out.println(ss);
         String trim = ss.trim();
         System.out.println(trim);
+        int number = 0;
+        String[] split =  ss.split(" ");
+        for(String s : split){
+            try {
+                int num = Integer.parseInt(s);
+                number = number + num;
+            }catch (Exception e){
+
+            }
+        }
+        System.out.println(number);
     }
 }
