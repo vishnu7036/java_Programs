ef5d6a68cdb7994c1e174ae911275bce674e031d
Vishnu <vishnug4881@gmail.com>
2024-03-16 12:06:47 +0530

    Latest Changes

--- src/main/java/collections/LinkedHashSetConcept.java
+++ src/main/java/collections/LinkedHashSetConcept.java
@@ -6,7 +6,7 @@ import java.util.LinkedHashSet;
 public class LinkedHashSetConcept {
     // It won't allow duplicates, but it will follow order of insertion
     public static void main(String[] args) {
-        LinkedHashSet lhs = new LinkedHashSet();
+        LinkedHashSet<Integer> lhs = new LinkedHashSet<>();
         lhs.add(10);
         lhs.add(50);
         lhs.add(20);
---
+++ src/main/java/seleniumRufLogics/FetchDataFromExcel.java
@@ -0,0 +1,4 @@
+package seleniumRufLogics;
+
+public class FetchDataFromExcel {
+}
