318fa9240d70cde3eb197a20339e01e0e4366651
vishnu <vishnu.g@mind-graph.com>
2024-07-24 15:41:31 +0530

    latest changes

--- src/main/java/programs/ReverseStringPartially.java
+++ src/main/java/programs/ReverseStringPartially.java
@@ -3,5 +3,14 @@ package programs;
 public class ReverseStringPartially {
     public static void main(String[] args) {
         String name = "vishnu pavan";
+        String[] names = name.split(" ");
+        String fn = names[0];
+        String ln = names[1];
+        String rev = "";
+        for (int i = fn.length() - 1; i >= 0; i--) {
+            rev = rev + fn.charAt(i);
+        }
+        String output = rev + " " + ln;
+        System.out.println(output);
     }
 }
